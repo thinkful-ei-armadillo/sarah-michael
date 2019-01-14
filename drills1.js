e1d03227dac7bf846be266c51ffa1acbad55b8b6
Sarah Drew <sarahdrew93@gmail.com>
2019-01-14 15:47:39 -0600

    added error

--- drills1.js
+++ drills1.js
@@ -1,11 +1,19 @@
 function getYearOfBirth(age){
-    return (2019 - age);
+if( age < 0){
+    throw new Error("Age can not be negative");
+}
+    return (2019 - age);  
 }
 
 function createGreeting(name, age){
     const yob = getYearOfBirth(age); 
     return `Hi, my name is ${name} and I'm ${age} years old and I was born in ${yob}.`; 
 } 
-    const greeting1 = createGreeting(); 
-    console.log(greeting1);
+try {
 
+  const greeting1 = createGreeting(); 
+    console.log(greeting1);
+} 
+catch(error) {
+    console.error(error);
+}
