3981d4445c59b076c38d4b6b3757353bd4d93a1b
Michael Bramble <mike.w.bramble@gmail.com>
2019-01-14 15:55:50 -0500

    changed drills1 again

--- drills1.js
+++ drills1.js
@@ -1,5 +1,6 @@
 function createGreeting(){
-    console.log('Hi, my name is Michael and I\'m 21 years old.');
+    return 'Hi, my name is Michael and I\'m 21 years old.';
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
