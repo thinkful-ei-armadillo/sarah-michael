c579ee1f92250ee494de61c03156f12db57fc753
Sarah Drew <sarahdrew93@gmail.com>
2019-01-14 16:17:06 -0600

    infinity problem

--- drills2.js
+++ drills2.js
@@ -4,3 +4,18 @@ function jediName(firstName, lastName){
 
     return lastName + firstName;
 }
+
+function beyond(num){
+    if(num === 0){
+        console.log('Staying home.')
+    }
+    if(num > 0 && num === Infinity){
+        console.log('To infinity');
+    }
+    if(num <0 && num === Infinity){
+        console.log('To negative inifinity');
+    }
+    if( num !== Infinity){
+        console.log('And beyond');
+    }
+}
