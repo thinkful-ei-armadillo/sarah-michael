6eeeb5b989596aa9c81e7c81baa07a5c6a782f4b
Sarah Drew <sarahdrew93@gmail.com>
2019-01-14 15:35:23 -0600

    added yearOfBirth

--- drills1.js
+++ drills1.js
@@ -1,6 +1,6 @@
 function createGreeting(name, age){
-    return `Hi, my name is ${name} and I'm ${age} years old.`;
-}
-
-const greeting1 = createGreeting();
-console.log(greeting1);
+    const yearOfBirth = (2019- age);
+     return `Hi, my name is ${name} and I'm ${age} years old and I was born in ${yearOfBirth}.`; 
+    } 
+    const greeting1 = createGreeting(); 
+    console.log(greeting1);
